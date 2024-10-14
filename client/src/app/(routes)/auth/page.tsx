34d8e3e04fa1207e90b707a0a25b55cc372df8bd
Alejandro Vera <alexvera0109@gmail.com>
2024-10-14 16:35:15 -0400

    added half placeholder

--- client/src/app/(routes)/auth/page.tsx
+++ client/src/app/(routes)/auth/page.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function Auth() {
   return (
-    <div>
+    <div className='flex'>
+      <div className='bg-yellow-500 h-screen w-[55%]'></div>
       <h1 className='text-center text-yellow-500 text-3xl font-bold'>Auth</h1>
     </div>
   )

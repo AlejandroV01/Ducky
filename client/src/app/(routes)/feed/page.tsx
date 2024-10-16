a18f9cb02563c012b2ddcb657059a228fc560f26
AlejandroV01 <alexvera0109@gmail.com>
2024-10-15 23:14:47 -0400

    styling minor changes

--- client/src/app/(routes)/feed/page.tsx
+++ client/src/app/(routes)/feed/page.tsx
@@ -1,10 +1,15 @@
+'use client'
 import PasswordStrength from '@/components/PasswordStrength'
-import React from 'react'
+import { Input } from '@/components/ui/input'
+import React, { useState } from 'react'
 export default function Feed() {
+  const [password, setPassword] = useState<string>('')
+
   return (
     <div className='flex flex-col items-center gap-5'>
       <h1 className='text-center text-yellow-500 text-3xl font-bold'>Feed</h1>
-      <PasswordStrength />
+      <Input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Enter Your Password' />
+      <PasswordStrength password={password} />
     </div>
   )
 }
--- client/src/components/PasswordStrength.tsx
+++ client/src/components/PasswordStrength.tsx
@@ -1,104 +1,67 @@
-"use client";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Check, X } from "lucide-react";
-import React, { useState } from "react";
-const PasswordStrength = () => {
-  const [password, setPassword] = useState<string>("");
-  const [progressValue, setProgressValue] = useState<number>(0);
+'use client'
+import { Progress } from '@/components/ui/progress'
+import { Check, X } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+
+const PasswordStrength = ({ password }: { password: string }) => {
+  const [progressValue, setProgressValue] = useState<number>(0)
 
   function containsSpecialChar(password: string): boolean {
-    const specialCharPattern = /[!@#$%^&*(),.?":{}|<>]/;
-    return specialCharPattern.test(password);
+    const specialCharPattern = /[!@#$%^&*(),.?":{}|<>]/
+    return specialCharPattern.test(password)
   }
   function calculateStrength(password: string): number {
-    let strength = 0;
+    let strength = 0
 
-    if (password.length >= 6) strength += 33.3;
-    if (/[0-9]/.test(password)) strength += 33.3;
-    if (containsSpecialChar(password)) strength += 33.3;
-    return strength;
+    if (password.length >= 6) strength += 33.3
+    if (/[0-9]/.test(password)) strength += 33.3
+    if (containsSpecialChar(password)) strength += 33.3
+    return strength
   }
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setPassword(value);
-    setProgressValue(calculateStrength(value));
-  };
+
+  useEffect(() => {
+    setProgressValue(calculateStrength(password))
+  }, [password])
+
   function strengthLevel(strength: number): {
-    level: string;
-    textColor: string;
+    level: string
+    textColor: string
   } {
     if (strength <= 34) {
-      return { level: "Weak", textColor: "text-red-500" };
+      return { level: 'Weak', textColor: 'text-red-500' }
     } else if (strength <= 67) {
-      return { level: "Moderate", textColor: "text-yellow-500" };
+      return { level: 'Moderate', textColor: 'text-yellow-500' }
     } else {
-      return { level: "Strong", textColor: "text-green-500" };
+      return { level: 'Strong', textColor: 'text-green-500' }
     }
   }
-  const { level, textColor } = strengthLevel(progressValue);
+  const { level, textColor } = strengthLevel(progressValue)
 
   return (
-    <div>
-      <Progress className="[&>*]:bg-[#8B6ED4]" value={progressValue} />
-      <Input
-        type="password"
-        value={password}
-        onChange={handleInputChange}
-        placeholder="Enter Your Password"
-      />
-      <h1 className={`md:font-bold ${textColor}`}>
-        Password Strength: {level}
+    <div className='flex flex-col gap-[0.305rem]'>
+      <Progress className='[&>*]:bg-[#8B6ED4] h-[12px]' value={progressValue} />
+      <h1 className={`font-semibold flex items-center gap-2`}>
+        Password Strength: <span className={`${textColor}`}>{level}</span>
       </h1>
-      <h1 className="md:font-bold">Your password must conatin</h1>
-      <div className="flex">
-        {password.length >= 6 ? (
-          <Check size={20} className="text-green-500" />
-        ) : (
-          <X size={20} className="text-neutral-700" />
-        )}
-        <p
-          className={
-            password.length >= 6 ? "text-green-500" : "text-neutral-700"
-          }
-        >
-          Password is at least 6 characters:
-        </p>
-      </div>
+      <h1 className='font-semibold'>Your password must contain:</h1>
+      <div className='flex flex-col gap-0'>
+        <div className='flex items-center'>
+          {password.length >= 6 ? <Check size={20} className='text-[#14B8A6]' /> : <X size={20} className='text-neutral-700' />}
+          <p className={password.length >= 6 ? 'text-[#14B8A6]' : 'text-neutral-700'}>Password is at least 6 characters:</p>
+        </div>
 
-      <div className="flex">
-        {/[0-9]/.test(password) ? (
-          <Check size={20} className="text-green-500" />
-        ) : (
-          <X size={20} className="text-neutral-700" />
-        )}
-        <p
-          className={
-            /[0-9]/.test(password) ? "text-green-500" : "text-neutral-700"
-          }
-        >
-          Contains one Number
-        </p>
-      </div>
+        <div className='flex items-center'>
+          {/[0-9]/.test(password) ? <Check size={20} className='text-[#14B8A6]' /> : <X size={20} className='text-neutral-700' />}
+          <p className={/[0-9]/.test(password) ? 'text-[#14B8A6]' : 'text-neutral-700'}>Contains one Number</p>
+        </div>
 
-      <div className="flex">
-        {containsSpecialChar(password) ? (
-          <Check size={20} className="text-green-500" />
-        ) : (
-          <X size={20} className="text-neutral-700" />
-        )}
-        <p
-          className={
-            containsSpecialChar(password)
-              ? "text-green-500"
-              : "text-neutral-700"
-          }
-        >
-          Contains one Special Character
-        </p>
+        <div className='flex items-center'>
+          {containsSpecialChar(password) ? <Check size={20} className='text-[#14B8A6]' /> : <X size={20} className='text-neutral-700' />}
+          <p className={containsSpecialChar(password) ? 'text-[#14B8A6]' : 'text-neutral-700'}>Contains one Special Character</p>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default PasswordStrength;
+export default PasswordStrength

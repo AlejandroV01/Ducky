e85c7a1d52e297c8d01c9d7acceb43f0ba9c6a2c
duranmichael681 <duranmichael681@gmail.com>
2024-10-15 23:21:37 -0400

    Changes to the Feed page that I was using to see the progress bar

--- client/src/app/(routes)/feed/page.tsx
+++ client/src/app/(routes)/feed/page.tsx
@@ -1,15 +1,9 @@
-'use client'
-import PasswordStrength from '@/components/PasswordStrength'
-import { Input } from '@/components/ui/input'
-import React, { useState } from 'react'
-export default function Feed() {
-  const [password, setPassword] = useState<string>('')
+import React from "react";
 
+export default function Feed() {
   return (
-    <div className='flex flex-col items-center gap-5'>
-      <h1 className='text-center text-yellow-500 text-3xl font-bold'>Feed</h1>
-      <Input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Enter Your Password' />
-      <PasswordStrength password={password} />
+    <div>
+      <h1 className="text-center text-yellow-500 text-3xl font-bold">Feed</h1>
     </div>
-  )
+  );
 }

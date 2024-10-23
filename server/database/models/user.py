1bb7fe999894d890c57ef91b71cf7f6a303a75c4
frank <franklin.neves.filho@gmail.com>
2024-10-23 16:39:56 -0400

    Updated User Class

--- server/database/models/user.py
+++ server/database/models/user.py
@@ -1,15 +1,15 @@
-from pydantic import BaseModel, EmailStr
-from uuid import UUID
+from pydantic import BaseModel, EmailStr, Field
+from uuid import UUID, uuid4
 from datetime import datetime
 
 class User(BaseModel):
-    id: UUID
+    id: UUID = Field(default_factory=uuid4)
     email: EmailStr
     first_name: str
     last_name: str
     user_name: str
     icon_url: str
-    created_on: datetime
+    created_on: datetime # datetime will be saved as datetime in the database
 
     class Config:
-        orm_mode = True
+        from_attributes = True

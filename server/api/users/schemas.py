01134cca572d986e3557aa79641d3479a33da8cc
frank <franklin.neves.filho@gmail.com>
2024-10-23 16:41:14 -0400

    added the UserResponse class to remove the password

--- server/api/users/schemas.py
+++ server/api/users/schemas.py
@@ -6,6 +6,19 @@ class CreateUser(BaseModel):
     last_name: str
     user_name: str
     icon_url: str
+    password: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
+
+class UserResponse(BaseModel):
+    id: str
+    email: str
+    first_name: str
+    last_name: str
+    user_name: str
+    icon_url: str
+    created_on: str
+
+    class Config:
+        from_attributes = True

7b8f79a49e87a8900e95793184621b4311e97060
frank <franklin.neves.filho@gmail.com>
2024-10-24 13:03:44 -0400

    deleted the update user class

--- server/api/users/schemas.py
+++ server/api/users/schemas.py
@@ -9,13 +9,3 @@ class CreateUser(BaseModel):
 
     class Config:
         from_attributes = True
-
-class UpdateUser(BaseModel):
-    email: EmailStr
-    first_name: str
-    last_name: str
-    user_name: str
-    icon_url: str
-
-    class Config:
-        from_attributes = True

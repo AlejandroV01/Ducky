00636f9991565f76d9970006357f128f28ec486f
frank <franklin.neves.filho@gmail.com>
2024-10-24 12:55:51 -0400

    added create_response function to simplify api requests

--- server/utils/response_generator.py
+++ server/utils/response_generator.py
@@ -16,3 +16,14 @@ def generate_response(data: Any = None, error: str = '', status: int = 200):
         status=status
     )
     return response.model_dump()  # Return the serialized response
+
+def create_response(response):
+    try:
+        if response.data:
+            obj = response.data[0]
+            return generate_response(obj, status=200)
+        else:
+            return generate_response(error="Object not found", status=404)
+    except Exception as e:
+        return generate_response(error=str(e), status=500)
+
